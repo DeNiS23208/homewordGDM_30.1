a5e2b3b663ca5215f3a269c5267dcd274e24d1f1
Denis Gulyaev <den23208@mail.ru>
2025-06-21 12:26:18 +0500

    edit homework

--- materials/models.py
+++ materials/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+
 class Course(models.Model):
     title = models.CharField(max_length=255)
     preview = models.ImageField(upload_to='course_previews/', blank=True, null=True)
     description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
 
     def __str__(self):
         return self.title
@@ -15,6 +20,10 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to='lesson_previews/', blank=True, null=True)
     video_url = models.URLField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
 
     def __str__(self):
         return f"{self.title} ({self.course.title})"
